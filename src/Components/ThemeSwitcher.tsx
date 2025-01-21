01e256cd8bbcd3ad22e411752932a0990a8e5fca
Your Name <enderson273014@gmail.com>
2025-01-20 20:00:18 -0400

    Tablas

--- src/Components/ThemeSwitcher.tsx
+++ src/Components/ThemeSwitcher.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch } from "antd";
+import { FaMoon, FaSun } from "react-icons/fa";
 
 interface ThemeSwitcherProps {
   isDarkMode: boolean;
@@ -12,8 +13,8 @@ const ThemeSwitcher: React.FC<ThemeSwitcherProps> = ({
 }) => {
   return (
     <Switch
-      checkedChildren="Claro"
-      unCheckedChildren="Oscuro"
+      checkedChildren={<FaSun style={{ color: "#FFD700" }} />}
+      unCheckedChildren={<FaMoon style={{ color: "#4B4B4B" }} />}
       checked={isDarkMode}
       onChange={toggleDarkMode}
     />
